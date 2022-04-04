a18dfc4189a37b9b44a61f619aecb5ccac26b218
robertolatini31 <robertolatini31@gmail.com>
2022-04-04 16:19:44 +0200

    added all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,17 +14,60 @@ const btnPlayElement = document.querySelector('.selectors>button');
 
 // creo la funzione al click del btn play
 btnPlayElement.addEventListener('click', function(){
+    //svuoto cells se pieno
+    document.querySelector('.cells').innerHTML = '';
     // collego livello di difficoltà inserito dall'utente
     const difficultyElement = document.querySelector('.selectors>select').value;
     // inizializzo la variabile in cui salvo l'intervallo
     let interval; 
+    // inizializzo la variabile per dare poi aspect ratio
+    let size;
     // selettore intervallo
     if(difficultyElement == 1) {
         interval = 100;
+        size = 'size_10';
     } else if (difficultyElement == 2) {
         interval = 81;
+        size = 'size_9';
     } else {
         interval = 49;
+        size = 'size_7';
     }
+    // chiamo la funzione che crea le cell e la salvo in una variabile
+    const cellElements = creatorSquareCell(interval, '.container_small>.cells', 'div', 'cell', size);
+    
+    // chiamo la funzione che aggiunge i numeri alle cell
+    const cellNumbers = addCellNumber('.cell', 'span');
+
+    // creo la funzionalità che colora di blu i quadrati al click
+
+    
 });
+
+
+// creo una funzione che genera tante cell quanto chiesto
+function creatorSquareCell(amount, query_selector, tagName, className, classSize) {
+    for (let i = 1; i <= amount; i++) { //implemento un ciclo per quante celle voglio fare
+        const cell = document.createElement(tagName); // creo dei div celle
+        cell.classList.add(className); // gli aggiungo la classe cell
+        cell.classList.add(classSize); // gli aggiungo la classe cell
+        document.querySelector(query_selector).append(cell); // metto la cell nel mio  html dentro cells  
+    }
+}
+
+// creo una funzione che aggiunge i numeri alle cell
+function addCellNumber(query_selector, tagName) {
+    const cells = document.querySelectorAll(query_selector);
+    let j = 1;
+    for(let i = 0; i < cells.length; i++) {
+        const cell = cells[i];
+        const spanElement = document.createElement(tagName);
+        spanElement.append(j);
+        cell.append(spanElement);
+        j++;
+        cell.addEventListener('click', function() {
+            cell.classList.add('bg_blu_click');
+        });
+    }
+}
 
